cdadc6509098bac0955075de56121e1ebb2e7f84
Alsu3912 <alsu3912@gmail.com>
2020-12-27 17:27:15 +0000

    NPM scripts

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('#mycarousel').carousel({ interval: 2000});
+    $('#carousel-button').click(function() {
+        if ($('#carousel-button').children('span').hasClass('fa-pause')) {
+            $('#mycarousel').carousel('pause');
+            $('#carousel-button').children('span').removeClass('fa-pause');
+            $('#carousel-button').children('span').addClass('fa-play');
+        } else {
+            $('#mycarousel').carousel('cycle');
+            $('#carousel-button').children('span').removeClass('fa-play');
+            $('#carousel-button').children('span').addClass('fa-pause');
+        };
+    });
+});
+$(document).ready(function() {
+    $('#reserve-button').click(function() {
+        $('#reserve-modal').modal({show: true});
+    });
+    $('#login-button').click(function() {
+        $('#login-modal').modal({show: true});
+    });
+})
